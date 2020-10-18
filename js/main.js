1193644fc4c178eb5ad48b630047f899b3a67260
Vladimir <vladimirkrizhanovski@gmail.com>
2020-10-18 16:45:19 +0700

    add_js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+    
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  });
+  
+  
+  $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 0,
+            showinfo : 0
+        },
+        
+    });
+
+    $('.heroes__slader-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+      });
+      $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slader-img',
+        fade: true,
+        arrows: false,
+      });
+});
+
